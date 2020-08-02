337640c365f8334c73f70355e5d5a4314771ccef
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-08-02 11:28:34 +0900

    RNA Transcription

---
+++ fsharp/rna-transcription/RnaTranscription.fs
@@ -0,0 +1,16 @@
+﻿module RnaTranscription
+
+open System
+
+let toRna (dna: string): string =
+    dna
+    |> Seq.toList
+    |> List.map (fun c ->
+        match c with
+        | 'G' -> 'C'
+        | 'C' -> 'G'
+        | 'T' -> 'A'
+        | 'A' -> 'U'
+        | _ -> failwith "unreachable!")
+    |> List.toArray
+    |> String
