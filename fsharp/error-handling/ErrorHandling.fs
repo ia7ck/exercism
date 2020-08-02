5711ccc7753f54dd89a974313f703c7bb53725b7
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-08-02 11:28:48 +0900

    Error Handling

---
+++ fsharp/error-handling/ErrorHandling.fs
@@ -0,0 +1,21 @@
+﻿module ErrorHandling
+
+let handleErrorByThrowingException () = raise (System.Exception("error"))
+
+let handleErrorByReturningOption (input: string) =
+    match System.Int32.TryParse input with
+    | (true, out) -> Some out
+    | (false, _) -> None
+
+let handleErrorByReturningResult (input: string) =
+    match System.Int32.TryParse input with
+    | (true, out) -> Ok out
+    | (false, _) -> Error "Could not convert input to integer"
+
+let bind switchFunction twoTrackInput =
+    match twoTrackInput with
+    | Ok x -> switchFunction x
+    | Error msg -> Error msg
+
+let cleanupDisposablesWhenThrowingException resource =
+    using resource (fun _ -> raise (System.Exception("error")))
