c627855986fd113ebc9d53e0159c29addb5fd74c
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-11-29 12:30:03 +0900

    diffie hellman

---
+++ fsharp/diffie-hellman/DiffieHellman.fs
@@ -0,0 +1,12 @@
+﻿module DiffieHellman
+
+open System
+
+let privateKey (primeP: bigint) =
+    let rand = Random()
+    rand.Next(2, int (primeP) - 1) |> bigint
+
+let publicKey (primeP: bigint) (primeG: bigint) (privateKey: bigint) = bigint.ModPow(primeG, privateKey, primeP)
+
+let secret (primeP:bigint) (publicKey:bigint) (privateKey:bigint) = 
+    bigint.ModPow(publicKey, privateKey, primeP)
