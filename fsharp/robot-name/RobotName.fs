a855634845c7e443a82f7333aab8283bfdb829f6
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-01-02 15:04:17 +0900

    Robot Name

---
+++ fsharp/robot-name/RobotName.fs
@@ -0,0 +1,29 @@
+﻿module RobotName
+
+open System
+open System.Collections.Generic
+
+let select (l: 'a list) =
+    let rng = Random()
+    List.item (rng.Next(0, l.Length)) l
+
+let set = HashSet<string>()
+
+let mkRobot () =
+    let robot =
+        Seq.initInfinite (fun _ ->
+            [ List.init 2 (fun _ -> select [ 'A' .. 'Z' ])
+              List.init 3 (fun _ -> select [ '0' .. '9' ]) ]
+            |> List.concat
+            |> String.Concat)
+        |> Seq.skipWhile (fun r -> set.Contains(r))
+        |> Seq.head
+
+    set.Add(robot) |> ignore
+    robot
+
+let name robot = robot
+
+let reset robot =
+    set.Remove(robot) |> ignore
+    mkRobot ()
