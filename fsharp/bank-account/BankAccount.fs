3aec8b5adcdca43b1754b6e1eb90739a54b86f2b
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-01-06 21:23:46 +0900

    Bank Account

---
+++ fsharp/bank-account/BankAccount.fs
@@ -0,0 +1,19 @@
+﻿module BankAccount
+
+type Account =
+    { mutable balance: decimal
+      valid: bool }
+    member this.getBalance() =
+        if this.valid then Some(this.balance) else None
+
+let mkBankAccount () = { balance = 0m; valid = false }
+
+let openAccount account = { account with valid = true }
+
+let closeAccount account = { account with valid = false }
+
+let getBalance (account: Account) = account.getBalance ()
+
+let updateBalance (change: decimal) (account: Account) =
+    lock account (fun () -> account.balance <- account.balance + change)
+    account
