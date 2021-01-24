3c5efc6adf5570596215ad0f028f8eb407e031e6
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-01-24 17:37:55 +0900

    Nth Prime

---
+++ fsharp/nth-prime/NthPrime.fs
@@ -0,0 +1,17 @@
+﻿module NthPrime
+
+let isPrime x =
+    match x with
+    | x when x <= 1 -> false
+    | 2 -> true
+    | x ->
+        let u = int (sqrt (float x)) + 1
+        seq { 2 .. u }
+        |> Seq.exists (fun y -> x % y = 0)
+        |> not
+
+let prime nth: int option =
+    Seq.initInfinite id
+    |> Seq.filter isPrime
+    |> Seq.take nth
+    |> Seq.tryLast
